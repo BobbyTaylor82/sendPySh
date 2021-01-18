83c64a35e447af6a05da2e8a665e0fd2e2dacf70
BobbyTaylor82 <bltxr9@gmail.com>
2021-01-18 10:46:27 -0600

    add files

---
+++ sendSH1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+      echo "BC Message: When are you going to work on installing Endevor on CA66. (userid)" >> send.txt
+      date >> send.txt
+      echo "-----------------------------------">> send.txt
+      echo " " >> send.txt
+
+      # git add *
+      # git commit -m "add new line of information"
+      # git push
+      # git pull
+
+
+      echo "Hey it worked" >> send.txt
+date >> send.txt
+echo "-----------------------------------">> send.txt
+echo " " >> send.txt
